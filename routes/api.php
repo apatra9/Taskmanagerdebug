20d518d287e7b67b6b1f6d6709c8259ae5af7e9e
Arunabha Patra <47187868+apatra9@users.noreply.github.com>
2020-08-31 18:10:25 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('tasks/{id}', 'TaskController@show');
-Route::get('tasks/{id}/members/{member}', 'TaskController@showmembertasks');
-Route::get('tasks','TaskController@showalive');
-Route::post('tasks/{id}','TaskController@func1');
-Route::patch('tasks/{id}', 'TaskController@patchitup');
+Route::get('tasks/{id}', 'TaskController@show');                                //Show single task by id
+Route::get('tasks/{id}/members/{member}', 'TaskController@showmembertasks');       //Show pending tasks for team and member
+Route::get('tasks','TaskController@showalive');                                    //Show all pending tasks
+Route::post('tasks/{id}','TaskController@func1');                                  //Add a task to a team
+Route::patch('tasks/{id}', 'TaskController@patchitup');                         //Mark a task as done
 
 
 
-Route::get('teams/{id?}', 'TeamController@show');
-Route::post('teams','TeamController@store');
-Route::post('teams/{id}/member', 'TeammembersController@func1');
+Route::get('teams/{id?}', 'TeamController@show');               //Show team by id
+Route::post('teams','TeamController@store');                    //Add a team
+Route::post('teams/{id}/member', 'TeammembersController@func1');   //Add a member to a team
 
 
